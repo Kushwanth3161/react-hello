1507c5b514cafe9b8da36884871d0a67c9574276
Kushwanth3161 <121281594+Kushwanth3161@users.noreply.github.com>
2023-07-29 14:22:31 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,26 @@
+ReactDOM.render(<div><h1>This is a react app.</h1>
+<p>React Js is first introduced by </p>
+<h2>Unordered List of Cars</h2>
+	<ul>
+		<li>Toyota Camry</li>
+		<li>Ford Mustang</li>
+		<li>Chevrolet Corvette</li>
+	</ul>
+
+	<h2>Ordered List of Brands</h2>
+	<ol>
+		<li>Toyota</li>
+		<li>Ford</li>
+		<li>Chevrolet</li>
+	</ol>
+
+	<h2>Description List of Inventors and Their Cars</h2>
+	<dl>
+		<dt>Henry Ford</dt>
+		<dd>Ford Model T</dd>
+		<dt>Soichiro Honda</dt>
+		<dd>Honda Civic</dd>
+		<dt>Karl Benz</dt>
+		<dd>Mercedes-Benz Patent-Motorwagen</dd>
+	</dl>
+    <img src="https://assets.vogue.in/photos/5d7224d50ce95e0008696c55/2:3/w_1920,c_limit/Joker.jpg" height="200px"/></div>,document.getElementById("content"));
